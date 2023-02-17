e72cfa2f69e86d2c47d5ba4a74d652d46e123231
vadim l <vadimlozinschii@gmail.com>
2023-02-17 09:26:40 -0800

    wrote 2 passing functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    let messages = [];
+    debugger;
+    let i = 0;
+    while (i < names.length) {  
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);       
+        i++;
+        debugger;
+    }
+    return messages;
+}
+writeCards(names);
+
+function countDown() {
+for (let coundown = 0; coundown < 11; coundown++) {
+    console.log(coundown);
+    }
+}
+
+countDown(10);
